cb38c95ce3e3067bc290341b8b04d7e7817fefde
Constanza Salinas de Lartundo <csdelartundo@gmail.com>
2024-09-12 11:17:26 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ListadoJuegos />
+  </div>
+</template>
+
+<script>
+import ListadoJuegos from './components/ListadoJuegos.vue'
+
+export default {
+  name: ' App',
+  components: {
+    ListadoJuegos
+  }
+}
+</script>
+
